b84264b9005641c6f30abdc44d6f9c60958c2e52
NovanHyperData <novan@hyperdatasolusindo.com>
2022-05-10 11:07:05 +0700

    Add GestureDetector to Home

--- lib/views/defaults/home/home.dart
+++ lib/views/defaults/home/home.dart
@@ -51,13 +51,29 @@ class HomeView extends StatelessWidget implements HomeViewContract {
                                 Icon(Icons.person, size: 70)
                               ],
                             ),
-                            Container(
-                              margin: EdgeInsets.only(bottom: 10),
-                              child: Text(
-                                  c.users.value > 1
-                                      ? 'Active Users'
-                                      : 'Active User',
-                                  style: TextStyle(fontSize: 15)),
+                            GestureDetector(
+                              onTap: () =>
+                                  Get.toNamed(RouteList.masterUser.index),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.green.shade200,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Text(
+                                        c.users.value > 1
+                                            ? 'Active Users'
+                                            : 'Active User',
+                                        style: TextStyle(fontSize: 18)),
+                                    Icon(
+                                      Icons.arrow_circle_right_outlined,
+                                      size: 30,
+                                    )
+                                  ],
+                                ),
+                              ),
                             )
                           ],
                         )))),
@@ -82,13 +98,29 @@ class HomeView extends StatelessWidget implements HomeViewContract {
                                 Icon(Icons.schedule, size: 70)
                               ],
                             ),
-                            Container(
-                              margin: EdgeInsets.only(bottom: 10),
-                              child: Text(
-                                  c.schedules.value > 1
-                                      ? 'Schedules Remaining'
-                                      : 'Schedule Remaining',
-                                  style: TextStyle(fontSize: 15)),
+                            GestureDetector(
+                              onTap: () =>
+                                  Get.toNamed(RouteList.ventesSchedule.index),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.blue.shade200,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Text(
+                                        c.schedules.value > 1
+                                            ? 'Schedules Remaining'
+                                            : 'Schedule Remaining',
+                                        style: TextStyle(fontSize: 18)),
+                                    Icon(
+                                      Icons.arrow_circle_right_outlined,
+                                      size: 30,
+                                    )
+                                  ],
+                                ),
+                              ),
                             )
                           ],
                         )))),
@@ -113,13 +145,29 @@ class HomeView extends StatelessWidget implements HomeViewContract {
                                 Icon(Icons.leaderboard, size: 70)
                               ],
                             ),
-                            Container(
-                              margin: EdgeInsets.only(bottom: 10),
-                              child: Text(
-                                  c.prospects.value > 1
-                                      ? 'Prospects Target'
-                                      : 'Prospect Target',
-                                  style: TextStyle(fontSize: 15)),
+                            GestureDetector(
+                              onTap: () =>
+                                  Get.toNamed(RouteList.ventesProspect.index),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.red.shade200,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Text(
+                                        c.prospects.value > 1
+                                            ? 'Prospects Target'
+                                            : 'Prospect Target',
+                                        style: TextStyle(fontSize: 18)),
+                                    Icon(
+                                      Icons.arrow_circle_right_outlined,
+                                      size: 30,
+                                    )
+                                  ],
+                                ),
+                              ),
                             )
                           ],
                         )))),
@@ -128,7 +176,7 @@ class HomeView extends StatelessWidget implements HomeViewContract {
                 sizes: ColScreen(sm: Col.col_3),
                 child: Container(
                     decoration: BoxDecoration(
-                      color: Colors.amber.shade300,
+                      color: Colors.purple.shade300,
                       borderRadius: BorderRadius.circular(10),
                     ),
                     child: Obx(() => Column(
@@ -144,13 +192,29 @@ class HomeView extends StatelessWidget implements HomeViewContract {
                                 Icon(Icons.domain, size: 70)
                               ],
                             ),
-                            Container(
-                              margin: EdgeInsets.only(bottom: 10),
-                              child: Text(
-                                  c.partners.value > 1
-                                      ? 'Business Partners'
-                                      : 'Business Partner',
-                                  style: TextStyle(fontSize: 15)),
+                            GestureDetector(
+                              onTap: () => Get.toNamed(
+                                  RouteList.masterBusinessPartner.index),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.purple.shade200,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Text(
+                                        c.partners.value > 1
+                                            ? 'Business Partners'
+                                            : 'Business Partner',
+                                        style: TextStyle(fontSize: 18)),
+                                    Icon(
+                                      Icons.arrow_circle_right_outlined,
+                                      size: 30,
+                                    )
+                                  ],
+                                ),
+                              ),
                             )
                           ],
                         )))),
