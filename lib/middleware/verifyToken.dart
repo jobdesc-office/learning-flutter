d594161cba21a9697d0422f8fb007d3d5bc46725
NovanHyperData <novan@hyperdatasolusindo.com>
2022-07-04 09:16:29 +0700

    Fix Processing When Session is Expired

--- lib/middleware/verifyToken.dart
+++ lib/middleware/verifyToken.dart
@@ -4,21 +4,24 @@ import 'package:get/get.dart';
 
 import '../constants/config_types.dart';
 import '../helpers/function.dart';
+import '../presenters/auth_presenter.dart';
 import '../routes/route_list.dart';
 import '../services/app_service.dart';
 import '../services/auth_service.dart';
 import '../utils/session_manager.dart';
 
 checkJwtToken() async {
-  final authPresenter = Get.find<AuthService>();
+  final authService = Get.find<AuthService>();
+  final authPresenter = Get.find<AuthPresenter>();
   final _nav = Get.find<NavigationPresenter>();
-  Response response = await authPresenter.verifyToken();
+  Response response = await authService.verifyToken();
 
   if (Get.currentRoute != RouteList.sigin.index) {
     if (response.statusCode == 401) {
       SessionManager.destroy();
-      authPresenter.signOut();
+      authService.signOut();
       _nav.darkTheme.value = false;
+      authPresenter.isProcessing.value = false;
       Snackbar().sessionExpired();
       toNameRoute(RouteList.sigin.index);
     }
