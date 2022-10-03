359c7bd62b2ed39fcca1fb3e471e50dd595a8742
NovanHyperData <novan@hyperdatasolusindo.com>
2022-10-03 08:24:58 +0700

    Fix Bug

--- lib/views/settings/company/company_setting/tabs/tabCustomizeField_source/_tabDailyActivity.dart
+++ lib/views/settings/company/company_setting/tabs/tabCustomizeField_source/_tabDailyActivity.dart
@@ -97,12 +97,30 @@ class _TabCustomizeFieldDailyActivity extends StatelessWidget
     sources.selectType.setSelected(BsSelectBoxOption(
         value: customField.custftype!.typeid,
         text: Text(customField.custftype!.typename.toString())));
-    sources.selectprospect.setSelected(BsSelectBoxOption(
-        value: customField.businesspartner!.bpid,
-        text: Text(customField.businesspartner!.bpname.toString())));
     sources.newprospect.value = customField.alldata ?? false;
     sources.visible.value = customField.onlythisdata ?? false;
     sources.inputName.text = customField.custfname ?? '';
+    if (customField.custfreftype?.typename == 'Prospect')
+      _FormCustomfield(ConfigType.prospectCustomField, 'Prospect').data =
+          'Prospect';
+    else
+      _FormCustomfield(ConfigType.activityCustomField, 'Activity').data =
+          'Activity';
+
+    if (customField.onlythisdata == true) {
+      sources.selectprospect.clear();
+      if (customField.custfreftype?.typename == 'Prospect') {
+        sources.selectprospect.setSelected(BsSelectBoxOption(
+            value: customField.refprospect?.prospectid,
+            text: Text(
+                '${customField.refprospect?.prospectname ?? ''} || ${customField.refprospect?.prospectcust?.sbccstmname ?? ''}')));
+      } else {
+        sources.selectprospect.setSelected(BsSelectBoxOption(
+            value: customField.refactivity?.dayactid,
+            text: Text(
+                '${customField.refactivity?.dayactloclabel ?? ''} || ${customField.refactivity?.dayactdate ?? ''}')));
+      }
+    }
 
     sources.createdby.value = customField.custfcreatedby?.userfullname ?? '';
     sources.createddate.value = customField.createddate ?? '';
